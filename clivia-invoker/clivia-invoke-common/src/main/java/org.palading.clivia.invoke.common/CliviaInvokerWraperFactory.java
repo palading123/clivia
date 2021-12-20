73b852c6229fcb423652562806a6bb6b07983ca8
palading_cr <palading_cr@163.com>
2021-12-20 21:34:06 +0800

    add license

--- clivia-invoker/clivia-invoke-common/src/main/java/org.palading.clivia.invoke.common/CliviaInvokerWraperFactory.java
+++ clivia-invoker/clivia-invoke-common/src/main/java/org.palading.clivia.invoke.common/CliviaInvokerWraperFactory.java
@@ -1,13 +1,24 @@
-/**
- * @author palading_cr
- * @title CliviaInvokerWraperFactory
- * @project clivia
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.palading.clivia.invoke.common;
 
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
+/**
+ * @author palading_cr
+ * @title CliviaInvokerWraperFactory
+ * @project clivia
+ */
 public interface CliviaInvokerWraperFactory {
      CliviaInvokerWraper create();
      Mono<Void> invoke(ServerWebExchange serverWebExchange,CliviaInvokerWraper cliviaInvokerWraper);
